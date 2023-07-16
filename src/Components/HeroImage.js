f2d82da961da6b1775372e415839a3a622ed9212
BNAS24 <124627177+BNAS24@users.noreply.github.com>
2023-07-16 09:11:01 -0400

    New updates 3

--- src/Components/HeroImage.js
+++ src/Components/HeroImage.js
@@ -66,108 +66,107 @@ const HeroImage = () => {
   };
 
   return (
-    <div id="heroCatch" className="hcatch">
-      <div className="video-container" style={{ position: 'relative' }}>
-        <video
-          ref={videoRef}
-          width="100%"
-          height="657"
-          autoPlay
-          muted={muted}
-          controls={false}
-          disablePictureInPicture
-          disableRemotePlayback
-          style={{
-            border: 'none',
-            outline: 'none',
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover',
-            objectPosition: 'center',
-            marginTop: '-32px',
-          }}
-        >
-          <source src="/Dune Part 2 Trailer.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <button
-          className={`mute-button circle ${isMuteButtonHovered ? 'hovered' : ''}`}
-          onClick={handleToggleMute}
-          onMouseEnter={handleMuteButtonMouseEnter}
-          onMouseLeave={handleMuteButtonMouseLeave}
-          style={{
-            position: 'absolute',
-            bottom: '80px',
-            right: '50px',
-            padding: '10px',
-            width: '40px',
-            height: '40px',
-            backgroundColor: 'transparent',
-            color: '#fff',
-            border: isMuteButtonHovered ? '1px solid white' : '1px solid rgba(217, 217, 217, 0.25)',
-            borderRadius: '50%',
-            cursor: 'pointer',
-            zIndex: '1',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            transition: 'border 0.3s ease, opacity 0.3s ease',
-          }}
-        >
-          {muted ? <VolumeOff /> : <VolumeOn />}
-        </button>
-        <button
-          className={`play-button ${isPlayButtonHovered ? 'hovered' : ''}`}
-          onClick={handleOpenFullscreen}
-          onMouseEnter={handlePlayButtonMouseEnter}
-          onMouseLeave={handlePlayButtonMouseLeave}
-          style={{
-            position: 'absolute',
-            bottom: '80px',
-            left: '50px',
-            padding: '10px',
-            width: '111px',
-            height: '40px',
-            backgroundColor: isPlayButtonHovered ? '#fff' : 'rgba(217, 217, 217, 0.25)',
-            color: '#fff',
-            border: 'none',
-            borderRadius: '7%',
-            cursor: 'pointer',
-            zIndex: '1',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            transition: 'background-color 0.3s ease, color 0.3s ease',
-          }}
-        >
-          <span className="play-text">Play</span>
-        </button>
-        <button
-          className={`my-list-button ${isMyListButtonHovered ? 'hovered' : ''}`}
-          onClick={handleMyListButtonClick}
-          onMouseEnter={handleMyListButtonMouseEnter}
-          onMouseLeave={handleMyListButtonMouseLeave}
-          style={{
-            color: '#fff',
-            backgroundColor: isMyListButtonHovered ? '#fff' : 'rgba(217, 217, 217, 0.25)',
-            border: 'none',
-            borderRadius: '7%',
-            padding: '10px',
-            width: '111px',
-            height: '40px',
-            position: 'absolute',
-            bottom: '80px',
-            left: '175px',
-            cursor: 'pointer',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            transition: 'background-color 0.3s ease',
-          }}
-        >
-          <span className="list-text">+ My List</span>
-        </button>
-      </div>
+
+    <div className="video-container">
+      <video
+        ref={videoRef}
+        width="100%"
+        height="657"
+        autoPlay
+        muted={muted}
+        controls={false}
+        disablePictureInPicture
+        disableRemotePlayback
+        style={{
+          border: 'none',
+          outline: 'none',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          objectPosition: 'center',
+          marginTop: '-32px',
+        }}
+      >
+        <source src="/Dune Part 2 Trailer.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <button
+        className={`mute-button circle ${isMuteButtonHovered ? 'hovered' : ''}`}
+        onClick={handleToggleMute}
+        onMouseEnter={handleMuteButtonMouseEnter}
+        onMouseLeave={handleMuteButtonMouseLeave}
+        style={{
+          position: 'absolute',
+          bottom: '80px',
+          right: '50px',
+          padding: '10px',
+          width: '40px',
+          height: '40px',
+          backgroundColor: 'transparent',
+          color: '#fff',
+          border: isMuteButtonHovered ? '1px solid white' : '1px solid rgba(217, 217, 217, 0.25)',
+          borderRadius: '50%',
+          cursor: 'pointer',
+          zIndex: '1',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          transition: 'border 0.3s ease, opacity 0.3s ease',
+        }}
+      >
+        {muted ? <VolumeOff /> : <VolumeOn />}
+      </button>
+      <button
+        className={`play-button ${isPlayButtonHovered ? 'hovered' : ''}`}
+        onClick={handleOpenFullscreen}
+        onMouseEnter={handlePlayButtonMouseEnter}
+        onMouseLeave={handlePlayButtonMouseLeave}
+        style={{
+          position: 'absolute',
+          bottom: '80px',
+          left: '50px',
+          padding: '10px',
+          width: '111px',
+          height: '40px',
+          backgroundColor: isPlayButtonHovered ? '#fff' : 'rgba(217, 217, 217, 0.25)',
+          color: '#fff',
+          border: 'none',
+          borderRadius: '7%',
+          cursor: 'pointer',
+          zIndex: '1',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          transition: 'background-color 0.3s ease, color 0.3s ease',
+        }}
+      >
+        <span className="play-text">Play</span>
+      </button>
+      <button
+        className={`my-list-button ${isMyListButtonHovered ? 'hovered' : ''}`}
+        onClick={handleMyListButtonClick}
+        onMouseEnter={handleMyListButtonMouseEnter}
+        onMouseLeave={handleMyListButtonMouseLeave}
+        style={{
+          color: '#fff',
+          backgroundColor: isMyListButtonHovered ? '#fff' : 'rgba(217, 217, 217, 0.25)',
+          border: 'none',
+          borderRadius: '7%',
+          padding: '10px',
+          width: '111px',
+          height: '40px',
+          position: 'absolute',
+          bottom: '80px',
+          left: '175px',
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          transition: 'background-color 0.3s ease',
+        }}
+      >
+        <span className="list-text">+ My List</span>
+      </button>
     </div>
   );
 };
